5517b436c81e2060cebc54ed88501333964dce7d
JoshuaBAtlas <84153010+JoshuaBAtlas@users.noreply.github.com>
2021-07-02 16:40:50 -0400

    Update run_the_app.py

--- run_the_app.py
+++ run_the_app.py
@@ -2,10 +2,10 @@ from tkinter import *
 from bsa_gui import Gui
 
 #There is one things that need to be done in manually within the code 
-#In the file tissue_grid in the first method it is shown where to update it
+#In the file bsa_gui in the second method (Second Window) you will see comments above code that uses the library cv2
 #When choosing a threshold value run the file thresh_value to help This is a GUI I found online
 #You can use this to find a good value for the thresh. It prints to the termial the current threshvalue you are seeing
-#Turn the bottom slider all the way to the LEFT ao that thresh type is Binary and the top bar is for the thresh number
+#Only change the three digit number (Second value from last) when changing the adaptiveThreshold
 #Id reccomned only clicking the ON/OFF button while the grid is showing on the picture with no channels (Show Grid)
 
 root = Tk()
